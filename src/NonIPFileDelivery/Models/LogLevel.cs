b123ce2155b30fafbc14a27cd06018768591952f
InvestorX <kinoco11+github@gmail.com>
2025-09-30 11:15:46 +0900

    Add LogLevel enum for logging levels

---
+++ src/NonIPFileDelivery/Models/LogLevel.cs
@@ -0,0 +1,12 @@
+namespace NonIPFileDelivery.Models;
+
+/// <summary>
+/// ログレベル（Serilogとマッピング）
+/// </summary>
+public enum LogLevel
+{
+    Debug = 0,
+    Info = 1,
+    Warning = 2,
+    Error = 3
+}
